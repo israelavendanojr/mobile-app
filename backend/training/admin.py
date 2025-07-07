63603309d423f0d97f6dfaf0e1f60ae1c2b0fa8c
israelavendanojr <iajr0207@gmail.com>
2025-07-06 20:32:36 -0700

    Fixed Migrations + Populating DB

--- backend/training/admin.py
+++ backend/training/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(UserPreferences)
+admin.site.register(Muscle)
+admin.site.register(Equipment)
+admin.site.register(ExercisePattern)
+admin.site.register(ExerciseMovement)
+admin.site.register(WorkoutDayTemplate)
+admin.site.register(WorkoutSplitTemplate)
+admin.site.register(SplitDayThrough)
+admin.site.register(DayPatternThrough)
--- backend/training/models.py
+++ backend/training/models.py
@@ -2,46 +2,71 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+# Generic models
+class Muscle(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+# Generation models
 class ExercisePattern(models.Model):
     name = models.CharField(max_length=100)  # e.g., "Vertical Pull"
-    target_muscles = models.JSONField(default=list)
-    equipment = models.JSONField(default=list)
-    order = models.IntegerField()
-
+    target_muscles = models.ManyToManyField(Muscle)
+    
     def __str__(self):
         return self.name
 
 class ExerciseMovement(models.Model):
     pattern = models.ForeignKey(ExercisePattern, on_delete=models.CASCADE, related_name="movements")
     name = models.CharField(max_length=100) # e.g., "Pull-up"
+    equipment = models.ManyToManyField(Equipment)
+    tags = models.JSONField(default=list)
     
     def __str__(self):
         return self.name
 
 class WorkoutDayTemplate(models.Model):
     name = models.CharField(max_length=100) # e.g., "Pull"
-    patterns = models.ManyToManyField(ExercisePattern)
+    patterns = models.ManyToManyField(
+        'ExercisePattern',
+        through='DayPatternThrough',
+        related_name='day_templates'
+    )
     
     def __str__(self):
         return self.name
 
-class WorkoutSplit(models.Model):
+class WorkoutSplitTemplate(models.Model):
     name = models.CharField(max_length=100) # e.g., "Push Pull Legs"
-    workouts = models.ManyToManyField(WorkoutDayTemplate, through='WorkoutSplitDay')
+    workouts = models.ManyToManyField(WorkoutDayTemplate, through='SplitDayThrough')
     days_per_week = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)])
     
     def __str__(self):
         return self.name
 
-# Through model to preserve order of workouts in split
-class WorkoutSplitDay(models.Model):
-    split = models.ForeignKey(WorkoutSplit, on_delete=models.CASCADE)
+# Through models
+class SplitDayThrough(models.Model):
+    split = models.ForeignKey(WorkoutSplitTemplate, on_delete=models.CASCADE)
     day_template = models.ForeignKey(WorkoutDayTemplate, on_delete=models.CASCADE)
     day_index = models.IntegerField() 
 
+class DayPatternThrough(models.Model):
+    day_template = models.ForeignKey(WorkoutDayTemplate, on_delete=models.CASCADE)
+    pattern = models.ForeignKey(ExercisePattern, on_delete=models.CASCADE)
+    pattern_index = models.IntegerField()
 
+    class Meta:
+        ordering = ['pattern_index']
+
+# User model
 User = get_user_model()
-# Stored preferences for workout generation
 class UserPreferences(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     days_per_week = models.IntegerField(default=2, validators=[MinValueValidator(1), MaxValueValidator(7)])
@@ -58,4 +83,3 @@ class UserPreferences(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Preferences"
 
-    
